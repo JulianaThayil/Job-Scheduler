ecb6dce49bfd7007ea58d25a8529c27456a22829
julianathayil <julianathayil@gmail.com>
2020-09-19 00:23:47 +0530

    Removed actions column from table
    changed the logic to skip all jobs by n days

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -68,26 +68,23 @@ const CustomDialog = (props) => {
         const HOLIDAY = ['2020-09-08'];
 
         jobs.map((job, index) => {
-            if (moment(job.startOn).isSameOrAfter(selectedJob)) { //postpone only those jobs which are after the selected job to be postponed not the ones before the selected job.
+            var counter = 0, momentDate = moment(new Date(job.startOn));
 
-                var counter = 0, momentDate = moment(new Date(job.startOn));
+            //postpones the job by n days only if its not a on a holiday nor a weekend
+            if (!HOLIDAY.includes(momentDate.format('YYYY-MM-DD')) && !WEEKEND.includes(momentDate.weekday())) {
+                while (counter < days) {
+                    momentDate = momentDate.add(1, 'days');
 
-                //postpones the job by n days only if its not a on a holiday nor a weekend
-                if (!HOLIDAY.includes(momentDate.format('YYYY-MM-DD')) && !WEEKEND.includes(momentDate.weekday())) {
-                    while (counter < days) {
-                        momentDate = momentDate.add(1, 'days');
-
-                        //skips postponing a job to a holiday or a weekend
-                        if (!HOLIDAY.includes(momentDate.format('YYYY-MM-DD')) && !WEEKEND.includes(momentDate.weekday())) {
-                            counter++
-                        }
+                    //skips postponing a job to a holiday or a weekend
+                    if (!HOLIDAY.includes(momentDate.format('YYYY-MM-DD')) && !WEEKEND.includes(momentDate.weekday())) {
+                        counter++
                     }
-
-                    //update the jobs state and set updated start date
-                    let newArr = [...jobs]
-                    newArr[index].startOn = momentDate.format('YYYY-MM-DD')
-                    setState(newArr)
                 }
+
+                //update the jobs state and set updated start date
+                let newArr = [...jobs]
+                newArr[index].startOn = momentDate.format('YYYY-MM-DD')
+                setState(newArr)
             }
         })
         setJobs(state);//update the jobs state in redux store
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 
@@ -13,13 +13,6 @@ import {
     Paper,
     Divider,
 } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-
-//custom
-import Dialog from "./Dialog";
-
-//icons
-import LowPriorityIcon from '@material-ui/icons/LowPriority';
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -50,27 +43,12 @@ const useStyles = makeStyles({
 const CustomizedTables = (props) => {
     const classes = useStyles();
     const { jobs } = props;
-    const [selectedJob, setSelectedJob] = useState();
-    const WEEKEND = [moment().day("Saturday").weekday(), moment().day("Sunday").weekday()];
-    const HOLIDAY = ['2020-09-08'];
-
-    //Dialog state and handlers
-    const [open, setOpen] = useState(false);
-
-    const handleDialogClose = () => {
-        setOpen(false);
-    };
-    const handleDialogOpen = (job) => {
-        setSelectedJob(job);
-        setOpen(true);
-    };
 
     return (
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="customized table">
                 <TableHead>
                     <TableRow>
-                        <StyledTableCell >Actions</StyledTableCell>
                         <StyledTableCell >Start On</StyledTableCell>
                         <StyledTableCell >Tasks</StyledTableCell>
                     </TableRow>
@@ -78,17 +56,6 @@ const CustomizedTables = (props) => {
                 <TableBody>
                     {jobs.map((job) => (
                         <StyledTableRow key={job.startOn}>
-
-                            <StyledTableCell>
-                                <IconButton disabled={WEEKEND.includes(moment(job.startOn).weekday()) || HOLIDAY.includes(moment(job.startOn).format('YYYY-MM-DD'))}
-                                    onClick={() => handleDialogOpen(job.startOn)}
-                                    color="primary"
-                                    label="postpone">
-                                    <LowPriorityIcon />
-                                </IconButton>
-
-                                <Dialog jobs={jobs} selectedJob={selectedJob} open={open} handleDialogClose={handleDialogClose} />
-                            </StyledTableCell>
 
                             <StyledTableCell component="th" scope="row">
                                 {moment(job.startOn).format('dddd')} {job.startOn}
